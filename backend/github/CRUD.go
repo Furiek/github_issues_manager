ff144b09e18c608832b0b81a28d2bfa04cc7ec52
Furiek <sebastian.malmqvist9@gmail.com>
2026-03-08 11:33:07 +0100

    updated project layout

--- backend/github/CRUD.go
+++ backend/github/CRUD.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // TODO: these have fairly similar sending procedures, can be grouped
@@ -69,4 +71,25 @@ func DeleteIssue(owner, repo string, issueNumber int) bool {
 	_ = repo
 	_ = issueNumber
 	return false
+}
+
+// SearchIssues queries the GitHub issue tracker.
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
 }
